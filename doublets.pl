8f10f389cbb4a976cf7238caa1b1d269a41499bd
Taylor <TaylorTheDeveloper@gmail.com>
2015-10-31 22:44:36 -0400

    final revisions

--- doublets.pl
+++ doublets.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/perl
 #Taylor Brockhoeft
 #Unix Tools
-#Friday, October 30th 2015
+#Friday, October 30nd 2015
 
 use strict;
-
-my @wordlist = ();#Loads the dictionary of same length words
 
 # READ AND HANDLE ARGS ==============================================================================
 my $count = 0;
 map { $count++ } @ARGV;
 
-if ($count != 2){ #UNCOMMENT THIS LATER
+if ($count != 2){ 
 	die "Must have a start and end word";
 }
 
@@ -25,13 +23,13 @@ if (length($start) != length($end)){
 }
 # END READ AND HANDLE ARGS ==========================================================================
 
-#Temporary for while working
-#my $ostart = "food";#Default start and end words
-#my $oend  = "gold";
-
+#Temporary for while working on project
+#my $start = "food";#Default start and end words
+#my $end  = "gold";
 
 # LOAD THE DICTIONARY FILE  =========================================================================
 my $filename = 'wordlist';
+my @wordlist = ();
 open(my $fh,'<',$filename) or die "Failed to load '$filename'";
 
 while ( my $row = <$fh> ) {
@@ -45,14 +43,11 @@ while ( my $row = <$fh> ) {
 close($fh);
 # END LOAD DICTIONARY ===============================================================================
 
-
-
 # TRY TO FIND PATH OF WORDS =========================================================================
-#initilize array queues for words to process
-#start = 0
-#end = 1
-
-#my @queue = ([$start],[$end]); #my @queue = ([[$word[0]], 'break'], [[$word[1]], 'break']);
+#initilize array queues for the start and end words to process
+#for the switch
+	# start = 0
+	# end = 1
 my @queue = ([[$start]], [[$end]]);
 
 #Create Two Word Candidates list so we can work back and forth between each side until we come to a solution
@@ -60,11 +55,10 @@ my @queue = ([[$start]], [[$end]]);
 my @candidates = ({$start => []}, {$end => []});
 #Swap between the start word and end word lists
 my $switch = 0; 
-#Contains (or not, I havent tests fake words) the path solution
-my @path;
-                        
+#Contains the path solution
+my @path;           
 
-while (1) { 
+until(@path){ # Until a path is reached
 
     my $w = shift @{$queue[$switch]};#grab first word on queue (start or end depending on switch)
 
@@ -77,30 +71,29 @@ while (1) {
 
 	    my $word;
 	    foreach $word (@similarwords) {
-	        if ($candidates[$switch ^ 1]{$word}) { #If it's in the candidate list for the other word, then we've made a complete path
+	        if ($candidates[$switch ^ 1]{$word}) { #If it's in the candidate hash for the other word, then we've made a complete path
 	            @path = (@$w, $word, reverse @{$candidates[$switch ^ 1]{$word}});
-	            print "solution found @path\n";
+	            #print "solution found @path\n";
 	        }
 
             if ($candidates[$switch]{$word}){#If it's in the candidate list for itself, skip it, we dont need it
-	        	#print "$word  already in\n";
+	        	#print "$word already in\n";
                 next;
             }
 
 	        $candidates[$switch]{$word} = [@$w];  #updatecandidate words
 	        
 	        push @{$queue[$switch]}, [@$w, $word];  #push new word to queue
-
 	    }
 	                                        
     }
     else{
-    	last; #DO A BARREL ROLL
+    	last; #DO A BARREL ROLL AND BAIL
     }
-
 }
+# END TRY TO FIND PATH OF WORDS =========================================================================
 
-
+# PRINT PATH ============================================================================================
 if (@path) {# found a path
 	pop @path;#remove the last element because we alreay know what it is and will make print much easier
     #print "$start --> ";
@@ -113,11 +106,10 @@ else{
 	print "no path found!\n";
 }
 
-
-
-
 exit 0; #done
+# END PRINT PATH ========================================================================================
 
+# FINDSIMILAR ===========================================================================================
 sub findsimilar{
 	#findsimilar compares a word to a list of words in wordlist, and returns ALL the one letter similar words. (for each char in the starting word)
 	#Init params 
@@ -126,22 +118,17 @@ sub findsimilar{
 	#Return Array of candid(ates)
 	my @candid = ();
 
-  		foreach my $query (@wordlist){ #For each word in the dictionary
-				if (issimilar($word,$query)!=-1){
-					#print $word;
-					my ($word, $i) = issimilar($word,$query);	
-					push(@candid,$word);
-				}
-				else{
-					#print "NOT \t$query\n";
-				}
-  		}#first for loop
-	
+	foreach my $query (@wordlist){ #For each word in the dictionary
+		if (issimilar($word,$query)!=-1){
+			#print $word;
+			my ($word, $i) = issimilar($word,$query);	
+			push(@candid,$word);
+		}
+	}
 
 	#use hashmap to delete dupliacate elements
 	my %unique = ();
-	foreach my $item (@candid)
-	{
+	foreach my $item (@candid){
 	    $unique{$item} ++;
 	}
 	my @uniquecandid = keys %unique;
@@ -152,7 +139,9 @@ sub findsimilar{
 
 	return @uniquecandid;
 }
+# END FINDSIMILAR ============================================================================================
 
+# ISSIMILAR ==================================================================================================
 sub issimilar{
 	#Check for one letter difference between two words
 	#returns the word and the index
@@ -162,8 +151,6 @@ sub issimilar{
 	my $index = 0;
 	my $count = 0;
 
-	#print "\n$word,$other\n";
-
 	for (my $i=0; $i < length $word; $i++) {
    			if ($wordsplit[$i] ne $othersplit[$i]){
    				$count += 1;
@@ -171,11 +158,11 @@ sub issimilar{
    			}
 	}
 	
-	if ($count==1){#If only one difference, then 
+	if ($count==1){#If only one difference, then it must be similar
 		#print "return\t$other\t$index";
 		return ($other,$index);
 	}
 
 	return -1;#Either more than one or no differences
 }
-
+# END ISIMILAR =================================================================================================
